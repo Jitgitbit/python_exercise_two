f5bf9a277ed01e76cf63a91b9705b0ebbbbb2d7b
Jitgitbit <rowowolf@gmail.com>
2020-06-18 14:32:04 +0200

    using URLs in Django

--- generator/views.py
+++ generator/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def home(request):
+	return HttpResponse('Hello hello ...')
+
+def eggs(request):
+	return HttpResponse('Eggs are great!')
