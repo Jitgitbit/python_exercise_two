b82f85d95a5cf2a8ae987913a73e2c23930f1ed5
Jitgitbit <rowowolf@gmail.com>
2020-06-18 15:20:09 +0200

    using the .choice method

--- generator/views.py
+++ generator/views.py
@@ -1,13 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import random
 
 # Create your views here.
 
 def home(request):
-	return render(request, 'generator/home.html', {'password': 'fffjyfttf336'})
+	return render(request, 'generator/home.html')
 
 def eggs(request):
 	return HttpResponse('<h1>Eggs are great!</h1>')
 
 def password(request):
-	return render(request, 'generator/password.html')
+
+	characters = list('abcdefghijklmnopqrstuvwxyz')
+
+	length = 10
+
+	thePassword = ''
+	for x in range(length):
+		thePassword += random.choice(characters)
+
+	return render(request, 'generator/password.html', {'password': thePassword})
